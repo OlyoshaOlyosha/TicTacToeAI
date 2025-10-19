7d7d68c79f1167dcdba88cbfd06a3a6c2beea25d
OlyoshaOlyosha <remaxux@proton.me>
2025-10-19 21:35:27 +0300

    refactor: обновлены параметры эволюции и наград для улучшения производительности

--- config.py
+++ config.py
@@ -1,24 +1,24 @@
 """Конфигурационные настройки проекта"""
 
 # Параметры эволюции
-POPULATION_SIZE = 100
-EPOCHS = 500
-ELITE_PCT = 0.3
-CROSSOVER_PCT = 0.6
-RANDOM_PCT = 0.1
-MUTATION_RATE = 0.003
+POPULATION_SIZE = 30
+EPOCHS = 100
+ELITE_PCT = 0.2
+CROSSOVER_PCT = 0.8
+RANDOM_PCT = 0.0
+MUTATION_RATE = 0.05
 
 # Параметры турнира
-NUM_OPPONENTS_RATIO = 0.2  # 20% от размера популяции
+NUM_OPPONENTS_RATIO = 0.5
 
 # Параметры сохранения и тестирования
-SAVE_TOP = 20
+SAVE_TOP = 30
 SAVE_FILENAME = "best.json"
 TEST_GAMES = 1000
 
 # Награда для подсчета очков
-WIN_SCORE = 100
-LOSS_SCORE = -150
-DRAW_SCORE = 50
-CENTER_BONUS = 30
-BLOCK_BONUS = 60
+WIN_SCORE = 60
+LOSS_SCORE = -50
+DRAW_SCORE = 40
+CENTER_BONUS = 20
+BLOCK_BONUS = 30
