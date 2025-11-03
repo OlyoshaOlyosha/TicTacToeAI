f1d94b562f7557e9c5b39ac8c7f7e5630f32e589
OlyoshaOlyosha <remaxux@proton.me>
2025-11-03 15:56:44 +0300

    refactor: упорядочены параметры сохранения и тестирования в config.py

--- config.py
+++ config.py
@@ -11,14 +11,14 @@ MUTATION_RATE = 0.05
 # Параметры турнира
 NUM_OPPONENTS_RATIO = 0.5
 
-# Параметры сохранения и тестирования
-SAVE_TOP = 30
-SAVE_FILENAME = "best.json"
-TEST_GAMES = 1000
-
 # Награда для подсчета очков
 WIN_SCORE = 60
 LOSS_SCORE = -50
 DRAW_SCORE = 40
 CENTER_BONUS = 20
 BLOCK_BONUS = 30
+
+# Параметры сохранения и тестирования
+SAVE_TOP = 30
+SAVE_FILENAME = "best.json"
+TEST_GAMES = 1000
